8e52ab97ec8b89362ff8b2ce7fc5a385b958809f
EC2 Default User <ec2-user@ip-172-31-23-147.ap-northeast-1.compute.internal>
2023-03-04 06:28:16 +0000

    [Fin]2-6-1

--- lesson6-1.rb
+++ lesson6-1.rb
@@ -2,6 +2,10 @@ puts "計算を始めます"
 puts "2つの値を入力してください"
 
 num1 = gets.to_i
-num2 = gets/to_i
+num2 = gets.to_i
 
-puts num1 * num2
+result = num1 * num2
+
+puts "計算結果を出力します"
+puts "#{num1}*#{num2}=#{result}"
+puts "計算を終了します"
